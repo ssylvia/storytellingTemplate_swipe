3fbe4b574ba89477bf0e75d6ae1bd29f9fa4d142
Stephen Sylvia <ssylvia@esri.com>
2012-12-11 15:22:33 -0500

    make swipe work better

--- forAGOL/forGallery/config.js
+++ forAGOL/forGallery/config.js
@@ -20,6 +20,12 @@
       "placeHolder": "1",
       "label": "Swipe Layer:"
     },{
+      "type": "paragraph",
+      "value": "Configure the layer you wish to swipe away by entering an integer between zero and the number of layers stored in your webmap (including your basemap). A number too large will display the following error: Unable to create map: Cannot read property ‘value’ of undefined."
+    },{
+      "type": "paragraph",
+      "value": "This application will combine all graphic layers (those stored as CSV, Feature Layer, Shapefile, Map Notes, etc.) into a single layer. Therefore, you cannot swipe away a single graphic’s layer to reveal a second graphics layer."
+    },{
       "type": "boolean",
       "fieldName": "legend",
       "label": "Display legend",
